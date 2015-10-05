d6f5add577fb493640b1515e30345b294d03fe0e
bmunroe91 <bmunroe91@gmail.com>
2015-10-04 20:54:03 -0500

    add basic-math.rb defining variable-spec.rb my-solution.rb variables-methods.rb

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,10 @@
+# Define an Empty Method
+def empty_method(name, middle_name, last_name)
+puts name
+puts middle_name
+puts last_name
+end
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
