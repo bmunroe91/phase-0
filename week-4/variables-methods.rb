c293b11a1a87f37fe2c56065d31d7f53f259311d
bmunroe91 <bmunroe91@gmail.com>
2015-10-04 22:25:19 -0500

    add reflection

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -14,3 +14,17 @@ num = gets.chomp
 better = num.to_i + 1
 
 puts 'This is a better number: ' + better.to_s + '.'
+
+# How do you define a local variable?
+# variable = "str" or float or integer etc...
+# How do you define a method?
+# def method(args
+#  end)
+# What is the difference between a local variable and a method?
+# A local variable is stored data to use in programs. A method is a function to combine # multiple statements into a single unit.
+# How do you run a ruby program from the command line?
+# ruby program.rb
+# How do you run an RSpec file from the command line?
+# rspec filename.spec
+# What was confusing about this material? What made sense?
+# I wasn't confused by any of this material so far.
