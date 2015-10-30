0590609371b703d987c4ae302974c24442af8589
bmunroe91 <bmunroe91@gmail.com>
2015-10-29 20:20:24 -0500

    add week-4 update

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -15,19 +15,21 @@
 
 
 # 1. total initial solution
-def total(array)
-  i = 0
-  answer = 0
-  while i < array.length
-    answer += array[i]
-    i += 1
-  end
-  return answer
-end
+# def total(array)
+#   i = 0
+#   answer = 0
+#   while i < array.length
+#     answer += array[i]
+#     i += 1
+#   end
+#   return answer
+# end
 
 
 # 3. total refactored solution
-
+def total(array)
+ array[0..-1].reduce(:+)
+end
 
 
 # 4. sentence_maker pseudocode
@@ -35,24 +37,28 @@ end
 # Input:
 # Output:
 # Steps to solve the problem.
-def sentence_maker(*words)
-  i = 0
-  sentence = ''
-  while i < words.length
-    if i == 0
-      sentence += words[0].to_s.capitalize + ' '
-    elsif i == words.length - 1
-      sentence += words[words.length - 1].to_s + '.'
-    elsif (i != words.length - 1) && (i != 0)
-      sentence += words[i].to_s.downcase + ' '
-    end
-    i += 1
-  end
-  return sentence
-end
-puts sentence_maker("this is a", "stringy", "sentence")
-# 5. sentence_maker initial solution
 
+
+# 5. sentence_maker initial solution
+# def sentence_maker(*words)
+#   i = 0
+#   sentence = ''
+#   while i < words.length
+#     if i == 0
+#       sentence += words[0].to_s.capitalize + ' '
+#     elsif i == words.length - 1
+#       sentence += words[words.length - 1].to_s + '.'
+#     elsif (i != words.length - 1) && (i != 0)
+#       sentence += words[i].to_s.downcase + ' '
+#      end
+#     i += 1
+#   end
+#   return sentence
+# end
+# puts sentence_maker("this is a", "stringy", "sentence")
 
 
 # 6. sentence_maker refactored solution
+def sentence_maker(array)
+ array.join(" ").capitalize << "."
+end
--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -6,9 +6,9 @@
 # Your Solution Below
 
 def leap_year?(year)
-  if year % 4 == 0 && year % 100 != 0 || % 400 == 0
-    puts true
+  if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+    return true
   else
-    puts false
+    return false
   end
 end
